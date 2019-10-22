ae207d0c0fa46a2e06e8076a30c37df09071480b
unknown <tanmaypandey1998@gmail.com>
2019-10-22 14:27:57 +0530

    renamed opencv_stuff to main

---
+++ main.py
@@ -0,0 +1,19 @@
+from utils import process_img
+import cv2
+import numpy as np
+from PIL import ImageGrab
+
+
+def main():
+    while True:
+        screen = np.array(ImageGrab.grab(bbox=(40, 16, 750, 600)))
+        try:
+            new_screen = process_img(screen)
+            cv2.imshow('window', new_screen)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+        except TypeError:
+            pass
+
+main()
